54c51676aecc5af6896697f234f30b7a6873282a
David McCann <mccann@work-book-pro.local>
2016-05-04 16:50:20 -0600

    dummy table view content

--- IV3Dm2-iOS/IV3Dm2-iOS/SelectSceneViewController.m
+++ IV3Dm2-iOS/IV3Dm2-iOS/SelectSceneViewController.m
@@ -19,6 +19,30 @@
     return self;
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 50;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString * CellIdentifier = @"Cell";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = @"All work and no play makes David a dull boy";
+    
+    return cell;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Hello World";
