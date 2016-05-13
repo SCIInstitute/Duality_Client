41712b1bccd6c83be8caa99cc914076489b036a4
David McCann <mccann@work-book-pro.local>
2016-05-13 10:07:37 -0600

    show stepper value in label

--- IV3Dm2-iOS/IV3Dm2-iOS/DynamicUIBuilder.mm
+++ IV3Dm2-iOS/IV3Dm2-iOS/DynamicUIBuilder.mm
@@ -21,14 +21,22 @@
 -(void) generateUI
 {
     [self clearUI];
-    for (const auto& floatParam : m_floatParams) {
-        UILabel* nameLabel = [self createNameLabel:floatParam];
+    for (size_t i = 0; i < m_floatParams.size(); ++i) {
+        UILabel* nameLabel = [self createNameLabel:m_floatParams[i]];
         [m_nameLabels addObject:nameLabel];
         [m_view addSubview:nameLabel];
         
-        UIStepper* stepper = [self createStepper:floatParam];
+        UIStepper* stepper = [self createStepper:m_floatParams[i] withTag:i];
         [m_steppers addObject:stepper];
         [m_view addSubview:stepper];
+
+        UILabel* valueLabel = [self createValueLabel];
+        [m_valueLabels addObject:valueLabel];
+        [m_view addSubview:valueLabel];
+    }
+    
+    for (UIStepper* stepper in m_steppers) {
+        [self stepToValue:stepper];
     }
 }
 
@@ -39,8 +47,8 @@
     CGFloat const height = 40;
     CGFloat const spacing = 2;
     
-    CGFloat const buttonWidth = 120;
-    CGFloat const labelWidth = 120;
+    CGFloat const stepperWidth = 120;
+    CGFloat const labelWidth = 80;
 
     for (int i = 0; i < [m_nameLabels count]; ++i) {
         CGFloat top = topMargin + i * (height + spacing);
@@ -49,7 +57,12 @@
     
     for (int i = 0; i < [m_steppers count]; ++i) {
         CGFloat top = topMargin + i * (height + spacing);
-        m_steppers[i].frame = CGRectMake(leftMargin + labelWidth, top, buttonWidth, height);
+        m_steppers[i].frame = CGRectMake(leftMargin + labelWidth, top, stepperWidth, height);
+    }
+
+    for (int i = 0; i < [m_valueLabels count]; ++i) {
+        CGFloat top = topMargin + i * (height + spacing);
+        m_valueLabels[i].frame = CGRectMake(leftMargin + labelWidth + stepperWidth, top, labelWidth, height);
     }
 }
 
@@ -64,6 +77,11 @@
         [stepper removeFromSuperview];
     }
     [m_steppers removeAllObjects];
+
+    for (UILabel* label in m_valueLabels) {
+        [label removeFromSuperview];
+    }
+    [m_valueLabels removeAllObjects];
 }
 
 -(UILabel*) createNameLabel:(const InputParameterFloat&)floatParam
@@ -71,21 +89,37 @@
     UILabel* label = [[UILabel alloc] init];
     label.text = [NSString stringWithUTF8String:floatParam.name.c_str()];
     label.textColor = [UIColor whiteColor];
-    label.backgroundColor = [UIColor colorWithWhite:0 alpha:0.8];
-    label.textAlignment = NSTextAlignmentCenter;
+    label.backgroundColor = [UIColor colorWithWhite:0 alpha:0];
+    label.textAlignment = NSTextAlignmentLeft;
     return label;
 }
 
--(UIStepper*) createStepper:(const InputParameterFloat&)floatParam
+-(UIStepper*) createStepper:(const InputParameterFloat&)floatParam withTag:(int)tag
 {
     UIStepper* stepper = [[UIStepper alloc] init];
     stepper.minimumValue = floatParam.lowerBound;
     stepper.maximumValue = floatParam.upperBound;
     stepper.stepValue = floatParam.stepSize;
     stepper.value = floatParam.defaultValue;
+    stepper.tag = tag;
+    [stepper addTarget:self action:@selector(stepToValue:) forControlEvents:UIControlEventValueChanged];
     return stepper;
 }
 
+-(UILabel*) createValueLabel
+{
+    UILabel* label = [[UILabel alloc] init];
+    label.textColor = [UIColor whiteColor];
+    label.backgroundColor = [UIColor colorWithWhite:0 alpha:0];
+    label.textAlignment = NSTextAlignmentRight;
+    return label;
+}
+
+- (void)stepToValue:(UIStepper*)stepper
+{
+    int index = stepper.tag;
+    m_valueLabels[index].text = [NSString stringWithFormat:@"%.02f", stepper.value];
+}
 
 @end
 
