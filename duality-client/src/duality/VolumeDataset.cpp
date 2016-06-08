bb152d44b41e80bea73f200d6ba87bf76ef38566
dmc <mccann.dmc@gmail.com>
2016-06-08 20:02:17 +0200

    computation of slice infos

--- duality-client/src/duality/VolumeDataset.cpp
+++ duality-client/src/duality/VolumeDataset.cpp
@@ -12,8 +12,25 @@ void VolumeDataset::read(std::shared_ptr<std::vector<uint8_t>> data) {
     ReaderFromMemory reader(reinterpret_cast<const char*>(data->data()), data->size());
     m_volume = std::make_unique<I3M::Volume>();
     I3M::read(reader, *m_volume);
+    initSliceInfos(m_volume->info);
 }
 
 void VolumeDataset::applyTransform(const IVDA::Mat4f& matrix) {
     // FIXME: how to apply transforms to volumetric dataset?
+}
+
+void VolumeDataset::initSliceInfos(const I3M::VolumeInfo& volumeInfo) {
+    IVDA::Vec3f vMin = volumeInfo.scale * -0.5f;
+    IVDA::Vec3f vMax = volumeInfo.scale * 0.5f;
+    for (size_t dir = 0; dir < 3; ++dir) {
+        for (size_t i = 0; i < volumeInfo.size[dir]; ++i) {
+            float normalizedPosInStack = static_cast<float>(i) / static_cast<float>(volumeInfo.size[dir] - 1);
+            float depth = vMin[dir] * (1.0f - normalizedPosInStack) + vMax[dir] * normalizedPosInStack;
+            float sliceIndex = normalizedPosInStack * (volumeInfo.size[dir] - 1);
+            size_t sliceIndex1 = std::min<size_t>(static_cast<size_t>(sliceIndex), volumeInfo.size[dir] - 1);
+            size_t sliceIndex2 = std::min<size_t>(sliceIndex1 + 1, volumeInfo.size[dir] - 1);
+            float interpolationParam = sliceIndex - sliceIndex1;
+            m_sliceInfos[dir] = SliceInfo{depth, sliceIndex1, sliceIndex2, interpolationParam};
+        }
+    }
 }
--- duality-client/src/duality/VolumeDataset.h
+++ duality-client/src/duality/VolumeDataset.h
@@ -3,6 +3,8 @@
 #include "src/duality/Dataset.h"
 #include "src/duality/I3M.h"
 
+#include <array>
+
 class VolumeDataset : public Dataset {
 public:
     VolumeDataset(Dataset::Visibility visibility);
@@ -13,6 +15,15 @@ private:
     virtual void read(std::shared_ptr<std::vector<uint8_t>> data) override;
     virtual void applyTransform(const IVDA::Mat4f& matrix) override;
 
+    void initSliceInfos(const I3M::VolumeInfo& volumeInfo);
+    struct SliceInfo {
+        float depth;
+        size_t textureIndex1;
+        size_t textureIndex2;
+        float interpolationParam;
+    };
+
 private:
     std::unique_ptr<I3M::Volume> m_volume;
+    std::array<SliceInfo, 3> m_sliceInfos;
 };
