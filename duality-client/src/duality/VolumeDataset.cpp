67ee0f70655e1260f7f47068d59b963fe0139da9
dmc <mccann.dmc@gmail.com>
2016-06-09 11:17:55 +0200

    create textures for volume dataset

--- duality-client/src/duality/VolumeDataset.cpp
+++ duality-client/src/duality/VolumeDataset.cpp
@@ -35,10 +35,59 @@ void VolumeDataset::initSliceInfos(const I3M::VolumeInfo& volumeInfo) {
             m_sliceInfos[dir].push_back(SliceInfo{depth, sliceIndex1, sliceIndex2, interpolationParam});
         }
     }
-    
+
     for (size_t dir = 0; dir < 3; ++dir) {
-        std::sort(begin(m_sliceInfos[dir]), end(m_sliceInfos[dir]), [](const SliceInfo& s1, const SliceInfo& s2) {
-            return s1.depth < s2.depth;
-        });
+        std::sort(begin(m_sliceInfos[dir]), end(m_sliceInfos[dir]),
+                  [](const SliceInfo& s1, const SliceInfo& s2) { return s1.depth < s2.depth; });
     }
+}
+
+void VolumeDataset::initTextures() {
+    for (size_t dir = 0; dir < 3; ++dir) {
+        int sizeU, sizeV, sizeW;
+        switch (dir) {
+        case 0:
+            sizeU = m_volume->info.size.y;
+            sizeV = m_volume->info.size.z;
+            sizeW = m_volume->info.size.x;
+            break;
+        case 1:
+            sizeU = m_volume->info.size.x;
+            sizeV = m_volume->info.size.z;
+            sizeW = m_volume->info.size.y;
+            break;
+        case 2:
+            sizeU = m_volume->info.size.x;
+            sizeV = m_volume->info.size.y;
+            sizeW = m_volume->info.size.z;
+            break;
+        }
+
+        std::vector<std::array<uint8_t, 4>> pixels(sizeU * sizeV);
+        for (size_t slice = 0; slice < sizeW; ++slice) {
+            for (size_t v = 0; v < sizeV; ++v) {
+                for (size_t u = 0; u < sizeU; ++u) {
+                    size_t index = 0;
+                    switch (dir) {
+                    case 0:
+                        index = texelIndexInVolume(slice, u, v);
+                        break;
+                    case 1:
+                        index = texelIndexInVolume(u, slice, v);
+                        break;
+                    case 2:
+                        index = texelIndexInVolume(u, v, slice);
+                        break;
+                    }
+                    pixels[v * sizeU + u] = {m_volume->voxels[index], m_volume->voxels[index + 1], m_volume->voxels[index + 2],
+                                             m_volume->voxels[index + 3]};
+                }
+            }
+        }
+        m_textures[dir].emplace_back(pixels.data(), GLTexture2D::TextureData::Color, sizeU, sizeV);
+    }
+}
+
+size_t VolumeDataset::texelIndexInVolume(size_t x, size_t y, size_t z) {
+    return x + y * m_volume->info.size.x + z * m_volume->info.size.x * m_volume->info.size.y;
 }
--- duality-client/src/duality/VolumeDataset.h
+++ duality-client/src/duality/VolumeDataset.h
@@ -2,6 +2,7 @@
 
 #include "src/duality/Dataset.h"
 #include "src/duality/I3M.h"
+#include "src/duality/GLTexture2D.h"
 
 #include <array>
 
@@ -15,15 +16,18 @@ private:
     virtual void read(std::shared_ptr<std::vector<uint8_t>> data) override;
     virtual void applyTransform(const IVDA::Mat4f& matrix) override;
 
-    void initSliceInfos(const I3M::VolumeInfo& volumeInfo);
     struct SliceInfo {
         float depth;
         size_t textureIndex1;
         size_t textureIndex2;
         float interpolationParam;
     };
+    void initSliceInfos(const I3M::VolumeInfo& volumeInfo);
+    void initTextures();
+    size_t texelIndexInVolume(size_t x, size_t y, size_t z);
 
 private:
     std::unique_ptr<I3M::Volume> m_volume;
     std::array<std::vector<SliceInfo>, 3> m_sliceInfos;
+    std::array<std::vector<GLTexture2D>, 3> m_textures;
 };
